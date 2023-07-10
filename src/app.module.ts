1b6543ec51d8bc0b231999eefde975e116b8fe23
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-07-10 14:23:19 +0100

    feat: Type ORM and Schedule ready in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PreordersModule } from './preorders/preorders.module';
 
 @Module({
-  imports: [PreordersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.STOCK_CHECKER_DATABASE_HOST,
+      port: 5432,
+      username: process.env.STOCK_CHECKER_DATABASE_USERNAME,
+      password: process.env.STOCK_CHECKER_DATABASE_PASSWORD,
+      database: process.env.STOCK_CHECKER_DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ScheduleModule.forRoot(),
+    PreordersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
