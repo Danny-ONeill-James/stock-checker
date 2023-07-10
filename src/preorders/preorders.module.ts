7daf62668e50d99e4d39bcf3256bb68f3a06ae9a
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-07-10 14:42:32 +0100

    feat: Now connects to database using env

--- src/preorders/preorders.module.ts
+++ src/preorders/preorders.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PreordersService } from './preorders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PreorderEntity } from './entities/preorder.entity';
 import { PreordersController } from './preorders.controller';
+import { PreordersService } from './preorders.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PreorderEntity])],
   providers: [PreordersService],
-  controllers: [PreordersController]
+  controllers: [PreordersController],
 })
 export class PreordersModule {}
