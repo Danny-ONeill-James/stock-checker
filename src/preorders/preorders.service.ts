3ae9800f6684385b421dd7ba6691a605347ab472
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-07-10 14:46:36 +0100

    feat: set up Create preorder DTO

--- src/preorders/preorders.service.ts
+++ src/preorders/preorders.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePreorderDto } from './dtos/CreatePreorder.dto';
+import { PreorderEntity } from './entities/preorder.entity';
 
 @Injectable()
-export class PreordersService {}
+export class PreordersService {
+  constructor(
+    @InjectRepository(PreorderEntity)
+    private preorderRepository: Repository<PreorderEntity>,
+  ) {}
+
+  async getFirestormPreorders(): Promise<CreatePreorderDto[]> {
+    return null;
+  }
+}
